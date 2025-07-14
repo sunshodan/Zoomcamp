183aaf856593db9d7905f9bd16a3dcdd94a57c3e
sunshodan <61916077+sunshodan@users.noreply.github.com>
2025-07-14 20:42:55 +0000

    lecture 2 mod 1 first 7 mins

--- docker_sql/pipeline.py
+++ docker_sql/pipeline.py
@@ -7,3 +7,30 @@ print(sys.argv)
 day=sys.argv[1]
 
 print(f'first pipeline on {day}')
+
+
+
+
+services:
+  postgres:
+    image: postgres:13
+    volumes:
+      - postgres-db-volume:/var/lib/postgresql/data
+    environment:
+      POSTGRES_DB: airflow
+      POSTGRES_USER: airflow
+      POSTGRES_PASSWORD: airflow
+    healthcheck:
+      test: ["CMD", "pg_isready","-U","airflow"]
+      interval: 5s
+      retries: 5
+    restart: always
+
+
+docker run -it \
+-e POSTGRES_DB=root \
+-e POSTGRES_USER=root \
+-e POSTGRES_PASSWORD=ny_taxi \
+-v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
+-p 5432:5432 \
+postgres:13
